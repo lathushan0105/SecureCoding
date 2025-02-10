a20d47461d72cbebe25bc9b8da916dc7abb3938d
Lathushan Pavalavelauthan <157456621+lathushan0105@users.noreply.github.com>
2025-02-10 01:41:36 -0500

    Update SimLab2_Ex1.c

--- SimLab2_Ex1.c
+++ SimLab2_Ex1.c
@@ -3,25 +3,38 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <fcntl.h>
+
 #define BUFSIZE 256
-    
+
 // This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
+    if (argc != 2) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    
-    char *runCMD[4] = {"wc", "-c"};
 
-    if (strlen(runCMD[0]) + strlen(runCMD[1]) + strlen(argv[1]) < BUFSIZE){
-        runCMD[2] = argv[1];
-        runCMD[3] = NULL;
-        execve("/usr/bin/wc", runCMD, NULL);
-        perror("execve failed");
+    // Open the file for reading
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("Failed to open the file");
+        return -1;
     }
+
+    // Redirect standard input
+    if (dup2(fd, STDIN_FILENO) < 0) {
+        perror("dup2 failed");
+        close(fd);
+        return -1;
+    }
+
+    close(fd);
+
+    char *runCMD[3] = {"wc", "-c", NULL};
+    execve("/usr/bin/wc", runCMD, NULL);
     
+    // If execve fails
+    perror("execve failed");
     return -1;
 }
