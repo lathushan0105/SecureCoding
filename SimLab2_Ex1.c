c94ff700fd8991cda8bac6416a616607df52c459
Lathushan Pavalavelauthan <157456621+lathushan0105@users.noreply.github.com>
2025-02-10 10:37:07 -0500

    Update SimLab2_Ex1.c

--- SimLab2_Ex1.c
+++ SimLab2_Ex1.c
@@ -15,8 +15,9 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    char fileName = argv[1];
     // Open the file for reading
-    int fd = open(argv[1], O_RDONLY);
+    int fd = open(fileName, O_RDONLY);
     if (fd < 0) {
         perror("Failed to open the file");
         return -1;
